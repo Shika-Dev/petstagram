01ba1f54be7ce9da56d809f59007889f0fe2f719
Gde Parama Artha Dharma <gededharma18@gmail.com>
2025-04-25 11:04:27 +0800

    Initial Commit

---
+++ petstagram/petstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  petstagramApp.swift
+//  petstagram
+//
+//  Created by Parama Artha on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct petstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ petstagramTests/petstagramTests.swift
@@ -0,0 +1,17 @@
+//
+//  petstagramTests.swift
+//  petstagramTests
+//
+//  Created by Parama Artha on 25/04/25.
+//
+
+import Testing
+@testable import petstagram
+
+struct petstagramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
